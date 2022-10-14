5c91efe91ef5551492773b8949f29a31d098a9f1
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-10-14 11:45:08 +0200

    fix: frame_too_large flakyness

--- interoperability/test_client/V5/test_publish.py
+++ interoperability/test_client/V5/test_publish.py
@@ -375,3 +375,21 @@ def test_subscribe_identifiers(base_wait_for):
                           callback2.messages[1][5].SubscriptionIdentifier[0]])
   assert received_subsids == expected_subsids
   bclient.disconnect()
+
+@pytest.mark.rlog_flaky
+def test_maximum_packet_size(base_wait_for, base_sleep, base_socket_timeout):
+  callback.clear()
+  # [MQTT-3.2.2-15]
+  # 1. server max packet size
+  myclient = mqtt_client.Client("test_maximum_packet_size")
+  myclient.registerCallback(callback)
+  connack = myclient.connect(host=host, port=port, cleanstart=True,
+                             socket_timeout=9 * base_socket_timeout)
+  assert hasattr(connack.properties, "MaximumPacketSize" )
+  payload = b"." * (int(connack.properties.MaximumPacketSize) + 1)
+  time.sleep(4 * base_sleep)
+  myclient.publish(topics[0], payload, 0)
+  # should get back a disconnect with packet size too big
+  waitfor(callback.disconnects, 1, 9 * base_wait_for)
+  assert len(callback.disconnects) == 1
+  assert callback.disconnects[0]["reasonCode"].value == 149
